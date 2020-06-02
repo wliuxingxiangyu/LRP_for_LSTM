826a1ab2da274ece7b4970416f4b50e02a86d3dd
aiden <wliuxingxiangyu@163.com>
2020-06-02 19:48:29 +0800

    before hz model backup

--- model/h5_print.py
+++ model/h5_print.py
@@ -3,19 +3,10 @@ import h5py
 from h5py import Dataset, Group, File
 import numpy as np
 import sys
-from keras.models import Sequential, load_model
+from keras.models import load_model
 
-sys.setrecursionlimit(10000) # 100000  Segmentation fault (core dumped)
-
-def recursive_ascii_encode(lst):
-    ret = []
-    for x in lst:
-        if isinstance(x, Group):  # covers both str and unicode
-            ret.append(x.encode('ascii', 'ignore'))
-        else:
-            ret.append(recursive_ascii_encode(x))
-            
-    return ret
+# sys.setrecursionlimit(10000) # 100000  Segmentation fault (core dumped)
+# usage: python2 h5_print.py
 
 # rnnSyscallNum = np.load("/home/hz/ws/share/gitlabshare/pyData-master/pyData/ins_log_syscallnum.h5")
 # print("rnnSyscallNum: %s" % str(rnnSyscallNum))
@@ -25,8 +16,6 @@ with h5py.File(rnnSyscallNumPath,'r') as f:
         print("f[fkey]: %s  \tfkey: %s  \tf[fkey].name: %s" % (f[fkey], fkey, f[fkey].name))
         print("type(fkey): %s" % type(fkey))
 
-    # a = f[str(fkey)][:]
-    # print("a: %s " % recursive_ascii_encode(f))
     print("--")
 
 
@@ -36,5 +25,6 @@ with h5py.File(rnnSyscallNumPath,'r') as f:
 model = load_model(rnnSyscallNumPath)
 for layer in model.layers:
     weights = layer.get_weights() # list of numpy arrays
-    print("weights : %s" % str(weights))
+    shape = np.shape(weights)
+    print("weights : shape:%s \n %s" % (str(shape), str(weights)))
 
